cddf0d4cf469d8314d29b004c486c8a7b3cff2b4
Aashiq <aashiqoffortune@gmail.com>
2025-11-07 01:46:18 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+
+#  Condtional statements if/else 
+
+# def function_name(parameters):
+#     """
+#     return value  # optional
+#  example 
+# def greet():
+#     print("Hello, world!")
+
+# greet()
