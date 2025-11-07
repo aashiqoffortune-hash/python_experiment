480910ba89ecab9140a340a994bf10d261b2c0dd
Aashiq <aashiqoffortune@gmail.com>
2025-11-07 14:43:23 +0530

    feat(calculator.py): add a simple calculator that performs basic arithmetic operations based on user input feat(hello.py): implement alcohol function to determine drink eligibility based on age and money, and add test cases for validation

---
+++ calculator.py
@@ -0,0 +1,17 @@
+
+a = float(input("Give me a number: "))
+b = input("Give me a operator: ")
+x = float(input("Give me yet another number: ")) 
+
+if b == "+":
+    print(a + x)
+elif b == "-":
+    print(a - x)
+elif b == "*":
+    print(a * x)
+elif b == "**":
+    print(a ** x)
+elif b == "/":
+    print(a / x)
+else:
+    print("Unknown operator.")
--- hello.py
+++ hello.py
@@ -1,11 +1,26 @@
+# def drink(money): 
+#     if money >= 2:
+#         return "You've got yourself a soup!"
+#     else:
+#         return "No soup for you!"
+    
 
-#  Condtional statements if/else 
+# print(drink(4))
+# print(drink(1))
 
-# def function_name(parameters):
-#     """
-#     return value  # optional
-#  example 
-# def greet():
-#     print("Hello, world!")
+def alcohol(age, money):
+    if (age >= 21) and (money >= 5):
+        return "We are getting a drink!"
+    elif (age >= 21) and (money < 5):
+        return "LOL you are not getting a drink, sorry!"
+    elif (age < 21) and (money >= 5):
+        return "Nice try kid!"
+    else:
+        return "You are too young and too poor"
 
-# greet()
+
+# Optional: test with other examples
+print(alcohol(21, 5))
+print(alcohol(21, 3))
+print(alcohol(18, 5))
+print(alcohol(18, 2))
